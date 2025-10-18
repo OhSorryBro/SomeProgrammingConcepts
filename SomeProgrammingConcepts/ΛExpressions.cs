363b5767e0079d303c1db2c480a0cebff78a2afc
Michal Domagała <maxisu1708@Interia.pl>
2025-10-18 20:51:57 +0200

    Lambda

--- SomeProgrammingConcepts/ΛExpressions.cs
+++ SomeProgrammingConcepts/ΛExpressions.cs
@@ -67,6 +67,16 @@ namespace SomeProgrammingConcepts
                 return b;
             };
 
+            Func<int, int> fibΛ = n =>
+            { if (n <= 1) return n;
+                int a = 0, b = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    int t = a + b; a = b; b = t;
+                }
+                return b;
+            };
+
             // Exercise 5 – LINQ using a named predicate
             bool Expensive(int x) { return x > 100; }
 
